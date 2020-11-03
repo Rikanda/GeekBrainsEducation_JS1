246168413c4bc3a0fa4551c261ede899de9c8065
Natalya <soltk7@yandex.ru>
2020-11-04 02:14:26 +0300

    5е задание

--- script.js
+++ script.js
@@ -30,52 +30,85 @@
 // alert(b); // 3
 
 
-// 2 задание
-var a = 2;
-var x = 1 + (a *= 2);
-alert("Во втором задании x=" +x);
+// // 2 задание
+// var a = 2;
+// var x = 1 + (a *= 2);
+// alert("Во втором задании x=" +x);
 
-//3 задание
-var a=+prompt("Третье задание. Введите любое целое число");
-var b=+prompt("Введите еще одно произвольное целое число");
+// //3 задание
+// var a=+prompt("Третье задание. Введите любое целое число");
+// var b=+prompt("Введите еще одно произвольное целое число");
 
-//проверка с помощью if...else
-if(a>=0){
-    if(b>=0)
-        alert(a-b);
-        else
-        alert(a+b);   
-}
-else if(b>=0)
-alert(a+b);
-else
-alert(a*b);
-
-
-// проверка с помощью тернарного оператора
-(a>=0&&b>=0)?(alert(a-b)):((a<0&&b<0)?alert(a*b):alert(a+b));
+// //проверка с помощью if...else
+// if(a>=0){
+//     if(b>=0)
+//         alert(a-b);
+//         else
+//         alert(a+b);   
+// }
+// else if(b>=0)
+// alert(a+b);
+// else
+// alert(a*b);
 
 
-// 4 задание
+// // проверка с помощью тернарного оператора
+// (a>=0&&b>=0)?(alert(a-b)):((a<0&&b<0)?alert(a*b):alert(a+b));
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+
+// // 4 задание
+
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   }
   
- var a = getRandomInt(0, 15);
- alert("Четвертое задание. a = " +a);
+//  var a = getRandomInt(0, 15);
+//  alert("Четвертое задание. a = " +a);
 
 
- function getNumbers(a){
-    var current = a;
-    var a_string = current + " ";
-  switch(15-a){
-       case 0:
-        return a_string;
-      default:
-          a_string +=getNumbers(++a);
-          return a_string;
+//  function getNumbers(a){
+//     var current = a;
+//     var a_string = current + " ";
+//   switch(15-a){
+//        case 0:
+//         return a_string;
+//       default:
+//           a_string +=getNumbers(++a);
+//           return a_string;
+//   }
+// }
+//   alert("Числовой ряд: " +getNumbers(a));
+
+  // 5е задание
+  var arg1= +prompt("Пятое задание. Введите первый аргумент:");
+  var arg2= +prompt("Введите второй аргумент:");
+
+  function mathSum(arg1, arg2) {
+  
+      return arg1+arg2;
+
   }
+
+ alert("Сумма аргументов: " + mathSum(arg1, arg2));
+
+  function mathRazn(arg1, arg2) {
+    return arg1-arg2;
+
 }
-  alert("Числовой ряд: " +getNumbers(a));
+
+ alert("Разность аргументов: " + mathRazn(arg1, arg2));
+
+ function mathUmn(arg1, arg2) {
+    return arg1*arg2;
+
+}
+
+ alert("Произведение аргументов: " + mathUmn(arg1, arg2));
+
+ function mathDel(arg1, arg2) {
+    return arg1/arg2;
+
+}
+
+ alert("Деление аргументов: " + mathDel(arg1, arg2));
     
